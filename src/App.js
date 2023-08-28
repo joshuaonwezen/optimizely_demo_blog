38933d19ca70c6a5aa9d77ff488bbc082350a304
Joshua Onwezen <joshua.onwezen@gmail.com>
2023-08-28 18:37:37 +0200

    add gtm notification listener push

--- src/App.js
+++ src/App.js
@@ -4,12 +4,25 @@ import {
   createInstance,
   OptimizelyProvider,
   useDecision,
+  enums
 } from '@optimizely/react-sdk';
 
 export default function App() {
   const optimizely = createInstance({
     sdkKey: 'XKkRw1SWTRnp5WXmVMYdB',
   })
+
+  const onDecision = ({ type, userId, attributes, decisionInfo }) => {
+      var dataLayerObject = window['dataLayer'] || [];
+      console.log(decisionInfo);
+      dataLayerObject.push({
+        'event': 'experience_impression',
+        'exp_variant_string': decisionInfo.ruleKey + '-' + decisionInfo.variationKey,
+      });
+  }
+  
+  const notificationId = optimizely.notificationCenter.addNotificationListener(enums.NOTIFICATION_TYPES.DECISION, onDecision);
+  
 
   return (
     <OptimizelyProvider optimizely={optimizely} user={{ id: 'user123' }}>
